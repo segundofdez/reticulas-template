15e34f9e808243a83976e4e5c8e2e3d371611eab
Segundo Fdez <segun.2@gmail.com>
2015-11-27 23:27:55 +0100

    update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
 // common
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var rename = require('gulp-rename');
-var clean = require('gulp-clean');
-var uglify = require('gulp-uglify');
+var del = require('del');
 var watch = require('gulp-watch');
-var livereload = require('gulp-livereload');
 var notify = require('gulp-notify');
 
 // styles
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
 
 // html
 var htmlmin = require('gulp-htmlmin');
@@ -20,9 +16,8 @@ var htmlmin = require('gulp-htmlmin');
 // image optimization
 var imagemin = require('gulp-imagemin');
 
-// test generate styleguide
-var styleguide = require('sc5-styleguide');
-var outputPath = 'styleguide';
+// synchronised browser
+var browserSync = require('browser-sync');
 
 
 /**
@@ -35,40 +30,45 @@ function swallowError (error) {
 
 
 /**
-* Task watch
+* Task browserSync: start server
 */
-gulp.task('watch', function () {
-    livereload.listen();
-    gulp.watch(['public/styles/**/*.less', 'public/styles/**/*.css'], ['styles']);
-    gulp.watch('public/js/**/*.js', ['js']);
-    gulp.watch('public/**/*.html', ['html']);
-    gulp.watch('public/media/img/**/*', ['images']);
-});
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: 'public'
+        },
+    })
+})
 
 
 /**
-* Html minified and livereload
-*/
-gulp.task('htmlclean', function() {
-    return gulp.src(['public/dist/**/*.html'])
-    .pipe(clean());
-});
-
-
-gulp.task('html',['htmlclean'], function() {
-    return gulp.src(['public/**/*.html'])
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('public/dist/'))
-    .pipe(livereload());
-});
-
-
-/**
-* Task styles: errors, autoprefixer, minified, rename, sourcemap, notify and livereload
+* Task styles: less, errors, autoprefixer, minified, rename, notify, browserSync
 */
 gulp.task('styles', function () {
     var less_src_import = 'public/styles/main.less';
     var less_dest_folder = 'public/styles/';
+
+    var minOpts = {processImport:true, keepSpecialComments:false};
+
+    return gulp.src(less_src_import)
+        .pipe(less())
+        .on('error', swallowError)
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(minifycss(minOpts))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(less_dest_folder))
+        .pipe(notify("Less compiled, prefixed and minified"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+
+/**
+* Task styles-dist: less, errors, autoprefixer, minified, rename, notify, browserSync
+*/
+gulp.task('styles-dist', function () {
+    var less_src_import = 'public/styles/main.less';
     var less_dest_dist_folder = 'public/dist/styles/';
 
     var minOpts = {processImport:true, keepSpecialComments:false};
@@ -78,87 +78,65 @@ gulp.task('styles', function () {
         .on('error', swallowError)
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(minifycss(minOpts))
-        .pipe(sourcemaps.init())
-            .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(less_dest_folder))
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(less_dest_dist_folder))
-        .pipe(notify("Less compiled, prefixed and minified"))
-        .pipe(livereload())
+        .pipe(notify({ message: 'Less minified' }));
 });
 
 
 /**
-* Task js concat and minified
+* Task images: optimize images to dist folder
 */
-gulp.task('js', function () {
-
-    var dest_folder = 'public/dist/js/';
-
-    gulp.src(dest_folder + '/*', {read: false}).pipe(clean({force: true}));
-
-    var src = [];
-
-    src.push('public/vendor/jQuery/dist/jquery.min.js');
-    src.push('public/js/main.js');
-
-    return gulp.src(src)
-        .on('error', swallowError)
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dest_folder))
-        .pipe(notify({message:"Js minified"})
-    );
-});
-
-
-/**
-* Task images optimize images with gulp-imagemin
-*/
-// Delete the build images directory
+// Delete dist folder images
 gulp.task('cleanimages', function() {
-    return gulp.src('public/dist/media/img/')
-    .pipe(clean());
+    return del([
+        'public/dist/media/img/**/*'
+    ]);
 });
 
-gulp.task('images',['cleanimages'], function () {
-    return gulp.src('public/media/img/**/*')
+gulp.task('images',['cleanimages'], function(){
+    var images_dest_folder = 'public/dist/media/img/';
+
+    return gulp.src('public/media/img/**/*.+(png|jpg|jpeg|gif|svg)')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}]
         }))
-        .pipe(gulp.dest('public/dist/media/img/'))
+        .pipe(gulp.dest(images_dest_folder))
         .pipe(notify({ message: 'Optimized images' }));
 });
 
 
 /**
-* Task styleguide to generate styleguide
+* Task html: minified html to dist folder
 */
-gulp.task('styleguide:generate', function() {
-  return gulp.src('public/styles/**/*.less')
-    .pipe(styleguide.generate({
-        title: 'My Styleguide',
-        server: true,
-        rootPath: outputPath,
-        overviewPath: 'README.md'
-      }))
-    .pipe(gulp.dest(outputPath));
+gulp.task('htmlclean', function() {
+    return del([
+        'public/dist/**/*.html'
+    ]);
 });
 
-gulp.task('styleguide:applystyles', function() {
-  return gulp.src('public/styles/main.less')
-    .pipe(less({
-      errLogToConsole: true
-    }))
-    .pipe(styleguide.applyStyles())
-    .pipe(gulp.dest(outputPath));
+gulp.task('html',['htmlclean'], function() {
+    return gulp.src(['public/**/*.html'])
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('public/dist/'))
+        .pipe(notify({ message: 'Html minified' }));
 });
-
-gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
 
 
 /**
-* Task for production
+* Task watch
 */
-gulp.task('prod', ['styles', 'js', 'html', 'images']);
+gulp.task('watch', ['browserSync'], function () {
+    gulp.watch(['public/styles/**/*.less'], ['styles']);
+    //gulp.watch(['public/styles/**/*.less', 'public/styles/**/*.css'], ['styles']);
+    gulp.watch('public/**/*.html', browserSync.reload);
+    //gulp.watch('public/js/**/*.js', ['js'], browserSync.reload);
+    //gulp.watch('public/media/img/**/*', ['images']);
+});
+
+
+/**
+* Task prod: Copy to dist folder for production
+*/
+gulp.task('prod', ['styles-dist', 'html', 'images']);
