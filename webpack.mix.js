170e37a6f938a509c4c9e93f5e2154286af58027
Segundo Fdez <segun.2@gmail.com>
2017-11-26 13:04:01 +0100

    Added imagemin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require('laravel-mix');
+let ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 /*
  |--------------------------------------------------------------------------
@@ -15,3 +18,27 @@ mix.js('src/js/main.js', 'public/js')
         'public/css/main.min.css')
     .browserSync('0.0.0.0:8181')
 ;
+
+//Only in production, you can remove if you want to use "npm run watch"
+if(mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new CopyWebpackPlugin([{
+                from: 'src/img',
+                to: 'public/img/',
+            }]),
+            new ImageminPlugin({
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                pngquant: {
+                    quality: '65-80'
+                },
+                plugins: [
+                    imageminMozjpeg({
+                        quality: 65,
+                        maxmemory: 1000 * 512
+                    })
+                ]
+            })
+        ],
+    });
+}
