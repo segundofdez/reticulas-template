dac9c7911a9aa1b39ea7b7548ee3f6d960446b38
Segundo Fdez <segun.2@gmail.com>
2017-12-20 00:36:18 +0100

    Added less and trying sourcemaps

--- new-static-site/webpack.config.js
+++ new-static-site/webpack.config.js
@@ -1,14 +1,65 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+/*
+var lessLoader = ExtractTextPlugin.extract(
+  "css?sourceMap!less?sourceMap"
+);
+*/
 var inProduction = (process.env.NODE_ENV === 'production');
 
+
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-       path: path.resolve(__dirname, './dist/js'),
-       filename: 'main.js'
+    entry: {
+        app: [
+            './src/js/main.js',
+            './src/styles/main.less'
+        ]
     },
-    plugins: []
+    output: {
+       path: path.resolve(__dirname, './dist'),
+       filename: './js/main.js'
+    },
+    //devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'less-loader']
+                    /*
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // If you are having trouble with urls not resolving add this setting.
+                                // See https://github.com/webpack-contrib/css-loader#url
+                                //url: false,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'less-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                    */
+
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './css/main.css'
+        }),
+        new webpack.LoaderOptionsPlugin ({
+            minimize: inProduction
+        })
+    ]
 };
 
 if (inProduction) {
