c5bc8d10dd752abbdbfe2f15b2625e46826af003
Segundo Fdez <segun.2@gmail.com>
2018-02-15 00:11:55 +0100

    added server and default folder for images

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 let ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 const imageminMozjpeg = require('imagemin-mozjpeg');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -22,18 +23,21 @@ mix.js('src/js/main.js', 'public/js')
     .combine([
         'node_modules/normalize.css/normalize.css',
         'public/css/main.css'],
-        'public/css/main.min.css')
-    .browserSync({
-        proxy: '0.0.0.0:8181',
-        files: ['public/*.html', 'public/css/*.css', 'public/js/*.js']
-    })
+        'public/css/main.min.css'
+    )
 ;
 
 mix.webpackConfig({
     plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            files: ['public/*.html', 'public/css/*.css', 'public/js/*.js'],
+            server: { baseDir: ['public'] }
+        }),
         new CopyWebpackPlugin([{
             from: 'src/img',
-            to: 'public/img', // Laravel mix will place this in 'public/img'
+            to: 'public/img',
         }]),
         new ImageminPlugin({
             test: /\.(jpe?g|png|gif|svg)$/i,
